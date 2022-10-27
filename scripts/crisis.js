c6d60efeeb069043af9e981fcde1fc51778db594
luchenqun <luchenqun@qq.com>
2022-10-27 20:32:51 +0800

    Update crisis.js

--- scripts/crisis.js
+++ scripts/crisis.js
@@ -3,13 +3,14 @@ import { decodeReply, execPromis } from "../utils.js";
 
 let run = async function () {
   try {
+    const app = process.platform == "win32" ? `.\\evmosd` : "./evmosd";
     const cwd = path.join(process.cwd(), "..");
     const fixed = `--from=node0 --home=./nodes/node0/evmosd/ --keyring-backend=test --chain-id=evmos_20191205-1 -y`;
     let cmd;
     let reply;
 
     {
-      cmd = `./evmosd tx crisis invariant-broken bank total-supply ${fixed}`;
+      cmd = `${app} tx crisis invariant-broken bank total-supply ${fixed}`;
       reply = await execPromis(cmd, { cwd });
       console.log(cmd, "\n", decodeReply(reply));
     }
