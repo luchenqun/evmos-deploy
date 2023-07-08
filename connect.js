5d1325bdfb5401a9168a566336e4cf11fbcc5126
Luke <chenqun.l@kaitaitech.cn>
2023-07-08 22:42:33 +0800

    update connect.js

--- connect.js
+++ connect.js
@@ -1,24 +1,18 @@
 import { createPromiseClient } from "@bufbuild/connect";
 import { Query } from "@buf/cosmos_cosmos-sdk.bufbuild_connect-es/cosmos/auth/v1beta1/query_connect.js";
 import { createGrpcTransport, createGrpcWebTransport, Http2SessionManager } from "@bufbuild/connect-node";
-
 // import { createConnectTransport } from "@bufbuild/connect-web";
 
-const sessionManager = new Http2SessionManager("http://127.0.0.1:9090", { pingIdleConnection: false });
 const transport = createGrpcTransport({
   baseUrl: "http://127.0.0.1:9090",
   httpVersion: "2",
-  sessionManager,
+  idleConnectionTimeoutMs: 10,
 });
 
 async function main() {
   const client = createPromiseClient(Query, transport);
-  // console.log(client.account);
-  let res;
-  res = await client.params({}, { timeoutMs: 1000 });
-  console.log(res);
-  // // process.exit(0);
-  sessionManager.abort("xxxxxxxx");
+  const res = await client.params({});
+  console.log(res.params);
 }
 
 main().catch((err) => {
