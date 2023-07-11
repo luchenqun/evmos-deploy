2c1c6649a56d9b5430f6b802df7e21eeb0689631
Luke <chenqun.l@kaitaitech.cn>
2023-07-11 15:09:21 +0800

    update staking.js

--- scripts/staking.js
+++ scripts/staking.js
@@ -7,25 +7,37 @@ let run = async function () {
     // you should use cmd `node init.js --v=4 --cn=1 --s=true` to run 5 nodes
 
     const cwd = path.join(process.cwd(), "..");
-    const privValidatorKeyPath = path.join(cwd, `nodes/node4/quarixd/config/priv_validator_key.json`);
-    const privValidatorKey = await fs.readJSON(privValidatorKeyPath);
-    const from = `node4`; // insure this address from node4/quarixd/key_seed.json, have great than 100000000000000000000 agov and aqrx
-    const pubkey = `'{"@type":"/cosmos.crypto.ed25519.PubKey","key":"${privValidatorKey.pub_key.value}"}'`; // usd cmd `./quarixd tendermint show-validator --home=./nodes/node4/quarixd`
-    const fixed = `--pubkey=${pubkey} --moniker="node4" --commission-rate="0.05" --commission-max-rate="0.10" --commission-max-change-rate="0.01" --min-self-delegation="100000000000000000000" --gas-prices 20000000000aqare --gas="auto" --from=${from} --home=./nodes/node4/quarixd/ --keyring-backend=test --chain-id=quarix_8888888-1 -y`;
+    const node = "node4"; // insure this address from ${node}/quarixd/key_seed.json, have great than 100000000000000000000 aqrx
+    const privValidatorKey = await fs.readJSON(path.join(cwd, `nodes/${node}/quarixd/config/priv_validator_key.json`));
+    const keySeedValidator = await fs.readJSON(path.join(cwd, `nodes/${node}/quarixd/key_seed.json`));
+    const valAddress = keySeedValidator.valAddress;
+    const ippId = 1;
+    const pubkey = `'{"@type":"/cosmos.crypto.ed25519.PubKey","key":"${privValidatorKey.pub_key.value}"}'`; // usd cmd `./quarixd tendermint show-validator --home=./nodes/${node}/quarixd`
+    const fixed1 = `--gas-prices 20000000000aqare --gas="auto" --from=${node} --home=./nodes/${node}/quarixd/ --keyring-backend=test --chain-id=quarix_8888888-1 -y`;
+    const fixed2 = `--gas-prices 20000000000aqare --gas="auto" --from=qoe --home=./nodes/node0/quarixd/ --keyring-backend=test --chain-id=quarix_8888888-1 -y`;
+
     let cmd;
     let reply;
 
     {
-      // // will fail, cause "failed to execute message; message index: 0: invalid coin denomination: got aquarix, expected agov: invalid request"
-      // cmd = `./quarixd tx staking create-validator --amount=100000000000000000000aqare ${fixed}`;
-      // reply = await execPromis(cmd, { cwd });
-      // console.log(cmd, "\n", decodeReply(reply));
-      //
-      // await sleep(1000 * 10);
-
-      cmd = `./quarixd tx staking create-validator --amount=100000000000000000000aqrx ${fixed}`;
+      cmd = `./quarixd tx staking allocate-investment-program-pool qoe ${valAddress} ${ippId} ${fixed2}`;
       reply = await execPromis(cmd, { cwd });
       console.log(cmd, "\n", decodeReply(reply));
+      await sleep(2000);
+    }
+
+    {
+      cmd = `./quarixd tx role assign-role Validator ${valAddress} ${fixed2}`;
+      reply = await execPromis(cmd, { cwd });
+      console.log(cmd, "\n", decodeReply(reply));
+      await sleep(2000);
+    }
+
+    {
+      cmd = `./quarixd tx staking create-validator --amount=90000000000000000000aqrx --commission-rate="0.05" --commission-max-rate="0.10" --commission-max-change-rate="0.01" --min-self-delegation="1" --pubkey=${pubkey} --moniker="${node}" ${fixed1}`;
+      reply = await execPromis(cmd, { cwd });
+      console.log(cmd, "\n", decodeReply(reply));
+      await sleep(2000);
     }
   } catch (error) {
     console.log("error", error);
