d17db8e696887c9eadba37d56fe5f08c40d9e59f
Luke <chenqun.l@kaitaitech.cn>
2023-10-05 16:26:29 +0800

    chore: update staking.js

--- scripts/staking.js
+++ scripts/staking.js
@@ -11,7 +11,7 @@ let run = async function () {
     const privValidatorKey = await fs.readJSON(privValidatorKeyPath);
     const from = `node1`; // insure this address from node1/evmosd/key_seed.json, have great than 100000000000000000000 agov and aevmos
     const pubkey = `'{"@type":"/cosmos.crypto.ed25519.PubKey","key":"${privValidatorKey.pub_key.value}"}'`; // usd cmd `./evmosd tendermint show-validator --home=./nodes/node1/evmosd`
-    const fixed = `--pubkey=${pubkey} --moniker="node1" --commission-rate="0.05" --commission-max-rate="0.10" --commission-max-change-rate="0.01" --min-self-delegation="1" --gas="600000" --from=${from} --home=./nodes/node1/evmosd/ --keyring-backend=test --chain-id=evmos_20191205-1 -y`;
+    const fixed = `--pubkey=${pubkey} --moniker="node1" --commission-rate="0.05" --commission-max-rate="0.10" --commission-max-change-rate="0.01" --min-self-delegation="1" --gas="600000" --gas-prices="10000000000aevmos" --from=${from} --home=./nodes/node1/evmosd/ --keyring-backend=test --chain-id=evmos_9000-1 --broadcast-mode sync -y`;
     let cmd;
     let reply;
 
@@ -23,7 +23,7 @@ let run = async function () {
 
       // await sleep(1000 * 10);
 
-      cmd = `./evmosd tx staking create-validator --amount=90000000000000000000aevmos ${fixed}`;
+      cmd = `./evmosd tx staking create-validator --amount=100000000000000000000aevmos ${fixed}`;
       reply = await execPromis(cmd, { cwd });
       console.log(cmd, "\n", decodeReply(reply));
     }
