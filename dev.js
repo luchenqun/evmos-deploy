2dde47aeb1e17204b3daa16f74f57fcacdb3a146
Luke <luchenqun@qq.com>
2024-11-22 17:08:06 +0800

    update

--- dev.js
+++ dev.js
@@ -441,7 +441,11 @@ const main = async function () {
           const genesisCfg = config.genesisCfg;
           if (Array.isArray(genesisCfg)) {
             for (const cfg of genesisCfg) {
-              eval('genesis.' + cfg);
+              try {
+                eval('genesis.' + cfg);
+              } catch (e) {
+                console.log(cfg)
+              }
             }
           }
 
